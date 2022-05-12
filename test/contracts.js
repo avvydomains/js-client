4259860ffe27246274a01e3268aa7f16d069bde3
Connor Bode <connor.bode@gmail.com>
2022-05-12 11:19:02 -0400

    verify contracts are provided by client

--- test/contracts.js
+++ test/contracts.js
@@ -4,9 +4,8 @@ import { ethers } from 'ethers'
 import AVVY from '../src/index.js'
 
 const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
-const TEST_NAME = 'avvy-client-common-testing.avax'
 
-describe('Names', async () => {
+describe('Contracts', async () => {
   let avvy
 
   beforeEach(async () => {
@@ -15,18 +14,8 @@ describe('Names', async () => {
     })
   })
 
-  it('should resolve a standard records', async () => {
-    const address = await avvy.name(TEST_NAME).resolve(AVVY.RECORDS.X_CHAIN)
-
-    // this is the sender of the first X-Chain transaction, after genesis
-    // see https://avascan.info/blockchain/x/tx/23y4DfgdAPj3teuJzrEfSXZkvmDpwYSbrmVHB82oLJGcMbXU8U
-    const expected = 'x-avax13fd740ykwc5peewmkcgu8r9nmnhns5gpdrgfjy'
-    expect(address).to.equal(expected)
-  })
-
-  it('should resolve a custom record', async () => {
-    const address = await avvy.name(TEST_NAME).resolve('CUSTOM_KEY')
-    const expected = 'CUSTOM_VALUE'
-    expect(address).to.equal(expected)
+  it('should provide contracts ', async () => {
+    const contracts = Object.keys(avvy.contracts)
+    expect(contracts.indexOf('ContractRegistryV1')).to.not.equal(-1)
   })
 })
