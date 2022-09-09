6890b8430fbe757722d7c4c3954df8512a7f449f
Connor Bode <connor.bode@gmail.com>
2022-09-09 17:35:09 -0400

    Add support for resolution of subdomains via parent domain resolvers; v2.5.0

--- src/index.js
+++ src/index.js
@@ -97,9 +97,8 @@ const Name = function (name, provider) {
         throw "Unknown key type passed to resolve()"
       }
 
-      let split = name.split('.')
-      let resolver 
       let domain = await getDomain() // this is the domain with 2 labels, e.g. name.avax
+      let nameHash = await utils.nameHash(name)
       let expiresAt = await provider.getExpiry(domain.hash)
       if (expiresAt === 0) {
         throw "Domain has not been registered"
@@ -109,15 +108,29 @@ const Name = function (name, provider) {
         throw "Domain registration is expired"
       }
 
-      const hash = await utils.nameHash(name)
-      try {
-        resolver = await provider.getResolver(domain.hash, hash)
-      } catch (err) {
-        throw "No resolver set"
+      // find the active resolver
+      // for a name aaa.bbb.ccc.avax, we must
+      // check for a resolver set at:
+      // - aaa.bbb.ccc.avax
+      // - bbb.ccc.avax
+      // - ccc.avax
+      // the resolver set at the longest subdomain is the one to use
+      let split = name.split('.')
+      let resolver
+      while (split.length >= 2) {
+        let subdomain = split.join('.')
+        let hash = await utils.nameHash(subdomain)
+        try {
+          resolver = await provider.getResolver(domain.hash, hash)
+          break
+        } catch (err) {}
+        split = split.slice(1)
       }
 
+      if (!resolver) throw "No resolver set"
+
       // fetch the value
-      return await resolveMethod(resolver.resolver, resolver.datasetId, hash, key)
+      return await resolveMethod(resolver.resolver, resolver.datasetId, nameHash, key)
     },
   }
 }
